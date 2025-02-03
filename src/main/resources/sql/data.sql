28400fad309d38434677010d4595e57c8ef836f0
mingeung <jmk2229@naver.com>
2025-02-03 23:50:23 +0900

    feat: datadase에 자동으로 값 입력되게 세팅

---
+++ src/main/resources/sql/data.sql
@@ -0,0 +1,7 @@
+INSERT INTO member (member_id, email, nickname, password) VALUES (1, 'email', '차차', 'password');
+
+
+INSERT INTO playing (track_id, member_id, date) VALUES ('attention', 1, now());
+INSERT INTO playing (track_id, member_id, date) VALUES ('attention', 1, now());
+INSERT INTO playing (track_id, member_id, date) VALUES ('hype boy', 1, now());
+
